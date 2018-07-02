0bca3c0936b6a95d01757ee4bf77cae1a5b0e7f8
Jussi Holtta <jussi.holtta@gmail.com>
2018-07-02 18:05:53 +0300

    chromeOS kludge devsetup

---
+++ gulpfile.js
@@ -0,0 +1,16 @@
+var gulp = require('gulp');
+var newer = require('gulp-newer');
+var sass = require('gulp-sass');
+
+var syncsource = '/data/data/com.termux/files/home/storage/downloads/html/learn247-gh-pages';
+var syncdestination = '/data/data/com.termux/files/home/learn247-gh-pages';
+
+gulp.task('sync', function() {
+	return gulp.src(syncsource + '/**/*')
+		.pipe(newer(syncdestination))
+		.pipe(gulp.dest(syncdestination));
+});
+
+gulp.task('watch', function() {
+	gulp.watch(syncsource + '/**/*', ['sync']);
+});
